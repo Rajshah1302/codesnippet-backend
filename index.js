557c2c5cc25a0894061b2b9ad53190ae2f952692
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2024-03-21 14:50:05 +0530

    Minor updates.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const express = require('express');
-const formSubmissionsRouter = require('./routes/formSubmissions');
-const cors = require('cors'); // Import the cors middleware
+const express = require("express");
+const formSubmissionsRouter = require("./routes/formSubmissions");
+const cors = require("cors"); // Import the cors middleware
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Mount the formSubmissionsRouter on '/api/form-submissions'
 app.use(formSubmissionsRouter);
 
 app.listen(5000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
