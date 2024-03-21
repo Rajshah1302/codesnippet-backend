10a6a0608d74b4340954348572acf140c163ffa5
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2024-03-21 16:15:48 +0530

    changed the port

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // Mount the formSubmissionsRouter on '/api/form-submissions'
 app.use(formSubmissionsRouter);
 
-app.listen(5000, () => {
+app.listen(6000, () => {
   console.log("Server is running on port 3000");
 });
 
